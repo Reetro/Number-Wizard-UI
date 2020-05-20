9023f62922d3865f258277611a40c40e3943567a
Reetro <thegamerboy7@gmail.com>
2020-05-19 22:25:10 -0600

    added files

---
+++ Assets/Scripts/Intro.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Intro : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI IntroText;
+
+    public NumberWizard WizardScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      string IntroTextStart = "Guess a number between " + WizardScript.min;
+      string IntroTextEnd = " and " + WizardScript.max;
+      string FinalText = IntroTextStart + IntroTextEnd;
+
+      IntroText.text = FinalText;
+    }
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+
+  [SerializeField] public int max = 1000;
+  [SerializeField] public int min = 1;
+  [SerializeField] TextMeshProUGUI GuessText;
+
+  int guess;
+
+  // Use this for initialization
+  void Start()
+  {
+    StartGame();
+  }
+
+  void StartGame()
+  {
+    NextGuess();
+  }
+
+  public void OnPressedHigher()
+  {
+    min = guess + 1;
+    NextGuess();
+  }
+  public void OnPressedLower()
+  {
+    max = guess - 1;
+    NextGuess();
+  }
+
+  void NextGuess()
+  {
+    guess = Random.Range(min, max + 1) / 2;
+    GuessText.text = guess.ToString();
+  }
+}
